4cadfb33cc549b6de807a0f17b3a6a618118eb5d
Stevin Prince <55503594+Kemosabe2911@users.noreply.github.com>
2020-02-21 22:42:19 +0530

    Updated Prime.java

--- Prime.java
+++ Prime.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;
+import java.util.Scanner;   //To import Scanner class
 public class Prime{
     public static void main(String args[]) {
         int n,i,flg=1;
-        Scanner s=new Scanner(System.in);
-        System.out.println("Enter the Number:");
-        n=s.nextInt();
+        Scanner s=new Scanner(System.in);          //Object creation
+        System.out.println("Enter the Number:"); 
+        n=s.nextInt();              //Read data
         for(i=2;i<=n/2;i++){
-            if(n%i==0){
+            if(n%i==0){             //Checking Prime or not
                 flg=0;
                 break;
             }
