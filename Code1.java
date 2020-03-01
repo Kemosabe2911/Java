7bf9d62e842e9b1806254cd11c4d8e106a8e9c0d
Kemosabe2911 <stevinprince29@gmail.com>
2020-03-01 21:17:13 +0530

    Update Code1

--- Code1.java
+++ Code1.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+//Code to find the log when n and e are given
+
+import java.util.Scanner; //Import Scanner class
 public class Code1  
 {  
     public static void main(String args[]) 
     { 
         int n,e;
         Scanner s= new Scanner(System.in);
-        System.out.println("Enter n:");
+        System.out.println("Enter n:"); 
         n=s.nextInt();
         System.out.println("Enter e:");
         e=s.nextInt(); 
